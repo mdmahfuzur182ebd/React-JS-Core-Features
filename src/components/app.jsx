84f31f23ff9a4685145ba348dd4c8fe51742e27b
mahfuzur <mdmahfuazur7788@gamil.com>
2023-07-26 18:25:54 +0600

    Our First React Component

---
+++ src/components/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <h1>Hello React, You are Awesome</h1>;
+  }
+}
+
+export default App;
