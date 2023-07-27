88378c0cb86189962f31a8e698ee454ecf4c5e99
mahfuzur <mdmahfuazur7788@gamil.com>
2023-07-27 22:49:35 +0600

    Modify Profile Component Using Props

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,27 +3,32 @@ import "../index.css";
 
 import Profile from "./profile";
 //import Skills from "./profile/skills";
-import  MyProps from './Props/index'
+import MyProps from "./Props/index";
+import Bio from "./profile/Bio";
+import Skills from "./profile/skills";
 
 class App extends Component {
   render() {
     return (
       <div className="some">
         <Profile />
+        <Bio name="SomeOne Developer " title="Java Developer" />
+
         <div style={{ marginTop: "30px", marginBottom: "30px" }}>
           <h3>List of Programmers</h3>
-          {/* <p>Mr. X</p>
-          <Skills />
+          <p>Mr. X</p>
+          <Skills skillA="Python" skillB=
+          "Data Science" skillC="NLP"
+          />
           <p>Mr. Y</p>
-          <Skills /> */}
-           <MyProps name="Md Mahfuzur Rahman"/>
-           <MyProps name="Something Do!"/>
+          <Skills skillA="NodeJs" skillB="ExpressJs" skillC="MongoDb"/>
+          <MyProps name="Md Mahfuzur Rahman" />
+          <MyProps name="Something Do!" />
         </div>
       </div>
     );
   }
 }
-
 
 export default App;
 //custom component taken attribute are call props
--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -6,12 +6,23 @@ import Skills from "./skills";
 import Links from "./Links";
 
 class Profile extends Component {
+  me ={
+    name:"Md Mahfuzr Rahman",
+    title: "Backend Fucus JavaScript Developer, Entrepreneur.",
+    skillA: 'Java',
+    skillB: 'Python',
+    skillC: 'NodeJs'
+  }
   render() {
     //console.log('Profile -', this.props)
     return (
       <div className="Container">
-        <Bio />
-        <Skills />
+        <Bio name={this.me.name} title={this.me.title} />
+        <Skills
+          skillA={this.me.skillA}
+          skillB={this.me.skillB}
+          skillC={this.me.skillC}
+        />
         <Links />
       </div>
     );
