fb8c25113720a79edb8ecc13251a1c433c265d72
mahfuzur <mdmahfuazur7788@gamil.com>
2023-07-27 20:28:26 +0600

    Spilt Profile Component into Multiple Child Components

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -10,47 +10,43 @@ const Bio = () => {
   );
 };
 
-const Skills = () => {
-  return (
-    <div className="Skills">
-      <h3>Skills:</h3>
-      <ul>
-        <li>Java</li>
-        <li>JavaScript</li>
-        <li>Python</li>
-      </ul>
-    </div>
-  );
-};
+const Skills = () => (
+  <div className="Skills">
+    <h3>Skills:</h3>
+    <ul>
+      <li>Java</li>
+      <li>JavaScript</li>
+      <li>Python</li>
+    </ul>
+  </div>
+);
 
-const Links = () => {
-  return (
-    <div className="Links">
-      <h3>Links:</h3>
-      <ul>
-        <li>
-          <a href="#">Facebook</a>
-        </li>
-        <li>
-          <a href="#">Twitter</a>
-        </li>
-        <li>
-          <a href="#">Linkedin</a>
-        </li>
-        <li>
-          <a href="#">Github</a>
-        </li>
-      </ul>
-    </div>
-  );
-};
+const Links = () => (
+  <div className="Links">
+    <h3>Links:</h3>
+    <ul>
+      <li>
+        <a href="#">Facebook</a>
+      </li>
+      <li>
+        <a href="#">Twitter</a>
+      </li>
+      <li>
+        <a href="#">Linkedin</a>
+      </li>
+      <li>
+        <a href="#">Github</a>
+      </li>
+    </ul>
+  </div>
+);
 class Profile extends Component {
   render() {
     return (
       <div className="Container">
         <Bio />
         <Skills />
-        <Links/>
+        <Links />
       </div>
     );
   }
