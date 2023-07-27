6e5ef6098a5ed73874320c46b8c06e4f0fbea4d6
mahfuzur <mdmahfuazur7788@gamil.com>
2023-07-27 21:02:25 +0600

    Reuse React Component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
+import "../index.css";
+
 import Profile from "./profile";
+import Skills from "./profile/skills";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="some">
         <Profile />
+        <div style={{ marginTop: "30px", marginBottom: "30px" }}>
+          <h3>List of Programmers</h3>
+          <p>Mr. X</p>
+          <Skills />
+          <p>Mr. Y</p>
+          <Skills />
+        </div>
       </div>
     );
   }
