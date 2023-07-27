3048a5bdedf1f0bc6c107f84352c39b9a7828624
mahfuzur <mdmahfuazur7788@gamil.com>
2023-07-27 20:18:41 +0600

    Spilt Profile Component1

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -1,39 +1,56 @@
 import React, { Component } from "react";
 import "./profile_style.css";
 
+const Bio = () => {
+  return (
+    <div className="Bio">
+      <h3>Md Mahfuzur Rahmna</h3>
+      <p>JavaScript Developer and code learning's</p>
+    </div>
+  );
+};
+
+const Skills = () => {
+  return (
+    <div className="Skills">
+      <h3>Skills:</h3>
+      <ul>
+        <li>Java</li>
+        <li>JavaScript</li>
+        <li>Python</li>
+      </ul>
+    </div>
+  );
+};
+
+const Links = () => {
+  return (
+    <div className="Links">
+      <h3>Links:</h3>
+      <ul>
+        <li>
+          <a href="#">Facebook</a>
+        </li>
+        <li>
+          <a href="#">Twitter</a>
+        </li>
+        <li>
+          <a href="#">Linkedin</a>
+        </li>
+        <li>
+          <a href="#">Github</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 class Profile extends Component {
   render() {
     return (
       <div className="Container">
-        <div className="Bio">
-          <h3>Md Mahfuzur Rahmna</h3>
-          <p>JavaScript Developer and code learning's</p>
-        </div>
-        <div className="Skills">
-          <h3>Skills:</h3>
-          <ul>
-            <li>Java</li>
-            <li>JavaScript</li>
-            <li>Python</li>
-          </ul>
-        </div>
-        <div className="Links">
-          <h3>Links:</h3>
-          <ul>
-            <li>
-              <a href="#">Facebook</a>
-            </li>
-            <li>
-              <a href="#">Twitter</a>
-            </li>
-            <li>
-              <a href="#">Linkedin</a>
-            </li>
-            <li>
-              <a href="#">Github</a>
-            </li>
-          </ul>
-        </div>
+        <Bio />
+        <Skills />
+        <Links/>
       </div>
     );
   }
