f92fe030f323240595b5bb486e95cef24137ef1e
mahfuzur <mdmahfuazur7788@gamil.com>
2023-07-29 19:32:45 +0600

    Introducing State in React other way

--- src/components/State/index.jsx
+++ src/components/State/index.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        count: 0
-    };
-  }
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//         count: 0
+//     };
+//   }
+    state = {
+        count : 0
+    }
 
   render() {
     return (
