311ce5a7152d951e084ce007b6f8654afad58771
mahfuzur <mdmahfuazur7788@gamil.com>
2023-07-29 18:42:21 +0600

    How to Render Children Props in React

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,28 +1,42 @@
 import React, { Component } from "react";
 import "../index.css";
 
-class Child extends Component {
-  render() {
-    this.props.func(this);
-    return (
+
+class Child extends Component{
+  render(){
+    this.props.func(this)
+    return(
       <div>
         <h1>I am child</h1>
       </div>
-    );
+    )
   }
 }
 
+const ChildComponent = props =>(
+  <div>
+      <h3>I am Child Component</h3>
+      <p>I don't know what to do </p>
+      {props.children}
+  </div>
+)
+
 class App extends Component {
+
   getContext(context) {
-    console.log(context);
+     console.log(context)
   }
 
   render() {
-    this.getContext(this);
+    
     return (
       <div className="some">
-        <h1>Pass Function as Props</h1>
-        <Child func={this.getContext} />
+         <h1>Pass Function as Props</h1>
+         {/* <Child func={this.getContext}/> */}
+         <ChildComponent>
+             <h2>Hello, I am From Parent</h2>
+             <h3>I am child of Child Component</h3>
+         </ChildComponent>
       </div>
     );
   }
