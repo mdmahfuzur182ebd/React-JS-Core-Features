2320e6e28985ad399ca8d28ac46480d9cc74925b
mahfuzur <mdmahfuazur7788@gamil.com>
2023-07-29 19:31:03 +0600

    Introducing State in React

--- src/components/State/index.jsx
+++ src/components/State/index.jsx
@@ -1,27 +1,27 @@
 import React, { Component } from "react";
 
-
 class App extends Component {
-    
-//     //1st way to state property old style
-//   constructor(props) {
-//     super(props);
-//     this.count = 5;
-//   }
-    // count = 0  other way
-     count = 0
+  constructor(props) {
+    super(props);
+    this.state = {
+        count: 0
+    };
+  }
 
   render() {
-   // console.log(this.count)
     return (
       <div>
         <h1>Why do we need state</h1>
-        <h2>Count: {this.count}</h2>
-        <button onClick={()=>{
-            this.count++;
-            console.log('clicked....', this.count)
-        }}>
-            Increment</button>
+        <h2>Count: {this.state.count}</h2>
+        <button
+          onClick={() => {
+            //this.count++;
+            this.setState({count: this.state.count + 1})
+            console.log("clicked....", this.state.count);
+          }}
+        >
+          Increment
+        </button>
       </div>
     );
   }
