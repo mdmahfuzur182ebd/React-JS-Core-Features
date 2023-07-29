9b2b09f29ca0a44a3ea635ae7f2ea9d005400ef0
mahfuzur <mdmahfuazur7788@gamil.com>
2023-07-29 20:34:24 +0600

    React State Example by Implementing A Timer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/State/index";
+import App from "./components/State/index1";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
