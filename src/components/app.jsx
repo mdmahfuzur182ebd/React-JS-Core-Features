9f1604c33564149638715a2961bc8f906cb799b6
mahfuzur <mdmahfuazur7788@gamil.com>
2023-07-30 23:44:03 +0600

    Text Field Change Event in React

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,20 +4,30 @@ import "../index.css";
 //Our First Event Handler-- event mean object -- event Name and function
 
 class App extends Component {
-
   handelButtonClick = (event) => {
-    console.log(event);
-    console.log('Button Click ..!')
+    //console.log(event.target);
+    console.log("Button Click ..!");
+  };
+
+  handleChange = event => {
+        console.log(event.target.value)
   };
 
   render() {
     return (
       <div className="some">
-        <div className="class Wrapper">
-          <h1 className=""> Event Is React</h1>
+        <div className="Wrapper">
+          <h1 className="Heading"> Event Is React</h1>
           <button className="btn" onClick={this.handelButtonClick}>
             Click Me!
           </button>
+          <hr />
+          <input
+            className="TestField"
+            type="text"
+            placeholder="Enter some Test"
+            onChange={this.handleChange}
+          />
         </div>
       </div>
     );
