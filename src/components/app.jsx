80e95006f815617afcdd5eec7af317edbc71f674
mahfuzur <mdmahfuazur7788@gamil.com>
2023-07-30 22:55:38 +0600

    First React Event Handler

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,49 +1,27 @@
 import React, { Component } from "react";
 import "../index.css";
 
-
-class Child extends Component{
-  render(){
-    this.props.func(this)
-    return(
-      <div>
-        <h1>I am child</h1>
-      </div>
-    )
-  }
-}
-
-const ChildComponent = props =>(
-  <div>
-      <h3>I am Child Component</h3>
-      <p>I don't know what to do </p>
-      {props.children}
-  </div>
-)
+//Our First Event Handler-- event mean object -- event Name and function
 
 class App extends Component {
 
-  getContext(context) {
-     console.log(context)
-  }
+  handelButtonClick = (event) => {
+    console.log(event);
+    console.log('Button Click ..!')
+  };
 
   render() {
-    
     return (
       <div className="some">
-         <h1>Pass Function as Props</h1>
-         {/* <Child func={this.getContext}/> */}
-         <ChildComponent>
-             <h2>Hello, I am From Parent</h2>
-             <h3>I am child of Child Component</h3>
-         </ChildComponent>
+        <div className="class Wrapper">
+          <h1 className=""> Event Is React</h1>
+          <button className="btn" onClick={this.handelButtonClick}>
+            Click Me!
+          </button>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
-
-//custom component taken attribute are call props
-// Pass function As props
-//p to child
